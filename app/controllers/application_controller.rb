20f9c93d20082673cca66eee5cfc4baff366d65d
Amber Taranto <ambzzz@gmail.com>
2019-10-07 19:15:34 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < Sinatra::Base
   configure do
     set :views, Proc.new { File.join(root, "../views/") }
     enable :sessions unless test?
+    # disable :show_exceptions
     set :session_secret, "secret"
   end
 
@@ -12,14 +13,29 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
 
+    @user = User.find_by(username: params[:username], password: params[:password])
+
+    if @user
+        session[:user_id] = @user.id
+        redirect '/account'
+    else
+      erb :error
+    end
   end
+  
+  #   rescue => exception
+  #     pp exception
+  #   end
+  # end
 
   get '/account' do
-
+    # @user = Helpers.current_user(session)
+    erb :account
   end
 
   get '/logout' do
-
+    session.clear
+    redirect to '/'
   end
 
 
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,20 @@
 class Helpers
+
+    def self.current_user(session)
+        @user = User.find(session[:user_id])
+        # user_id = session[:user_id]
+        # user = User.find(user_id)
+        # user
+    end
+
+
+    def self.is_logged_in?(session)
+        !!session[:user_id]
+        # if session[:user_id]
+        #     return true
+        # else 
+        #     return false
+        # end
+    end
+
 end
